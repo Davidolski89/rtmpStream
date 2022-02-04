755e2cebc7aa2ffe2cc82ea78f0ffaa9d8e46cba
Davidolski89 <98594474+Davidolski89@users.noreply.github.com>
2022-02-04 11:54:15 +0100

    Important controller

---
+++ Controller/InfoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using rtmpStream.Data;
+
+namespace rtmpStream.Controller
+{
+    public class InfoController : ControllerBase
+    {
+        StreamChecker checker;
+        public InfoController(StreamChecker streamchecker)
+        {
+            checker = streamchecker;
+        }
+
+        [Route("api/[controller]")]        
+        [HttpGet]
+        public IEnumerable<string> GetStreams()
+        {
+            return checker.oldList;
+        }
+    }
+}
